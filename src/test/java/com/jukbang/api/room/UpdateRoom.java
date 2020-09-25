c7d40eb78b7e39c04426cb494df0f812d235a700
always0ne <si8363@naver.com>
2020-09-25 18:23:26 +0900

    refactor: Exception Handler 누락 추가

--- src/test/java/com/jukbang/api/room/UpdateRoom.java
+++ src/test/java/com/jukbang/api/room/UpdateRoom.java
@@ -34,4 +34,41 @@ public class UpdateRoom extends BaseControllerTest {
                 .andDo(document("updateRoom"))
         ;
     }
+
+    @Test
+    @WithMockUser("TestUser1")
+    @DisplayName("방 수정하기 방이 없을 때 (실패)")
+    void updateRoomFailBecauseNotFound() throws Exception {
+        String accessToken = userFactory.signUpUser(1, UserRole.ROLE_LANDLORD).getAccessToken();
+        Long roomId = roomFactory.generateRoom("TestUser" + 1);
+
+        UpdateRoomRequest updateRoomRequest = roomFactory.generateUpdateRoomRequest();
+
+        this.mockMvc.perform(RestDocumentationRequestBuilders.put("/landlord/rooms/{roomId}", roomId+1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + accessToken)
+                .content(this.objectMapper.writeValueAsString(updateRoomRequest)))
+                .andExpect(status().isNotFound())
+                .andDo(print())
+        ;
+    }
+
+    @Test
+    @WithMockUser("TestUser1")
+    @DisplayName("방 수정하기 수정 권한이 없을 때 (실패)")
+    void updateRoomFailBecauseNotMyRoom() throws Exception {
+        userFactory.signUpUser(1, UserRole.ROLE_LANDLORD);
+        String accessToken = userFactory.signUpUser(2, UserRole.ROLE_LANDLORD).getAccessToken();
+        Long roomId = roomFactory.generateRoom("TestUser" + 1);
+
+        UpdateRoomRequest updateRoomRequest = roomFactory.generateUpdateRoomRequest();
+
+        this.mockMvc.perform(RestDocumentationRequestBuilders.put("/landlord/rooms/{roomId}", roomId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + accessToken)
+                .content(this.objectMapper.writeValueAsString(updateRoomRequest)))
+                .andExpect(status().isForbidden())
+                .andDo(print())
+        ;
+    }
 }
